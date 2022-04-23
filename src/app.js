9ce41f731bbda5519027ddd357c758ec01adaa2e
Asanali12 <sapswung@gmail.com>
2022-04-24 00:44:57 +0300

    changed build script

--- src/app.js
+++ src/app.js
@@ -24,28 +24,7 @@ router.get("/requests", (req, res) => {
 
 console.log("here")
 
-const crm = new AmoCRM({
-  // логин пользователя в портале, где адрес портала domain.amocrm.ru
-  domain: 'aurorainc', // может быть указан полный домен вида domain.amocrm.ru, domain.amocrm.com
-  /* 
-    Информация об интеграции (подробности подключения 
-    описаны на https://www.amocrm.ru/developers/content/oauth/step-by-step)
-  */
-  auth: {
-    client_id: 'f8cef8e7-570a-458d-bd63-17d78b560f2e', // ID интеграции
-    client_secret: '4gWhvCDtNEkycxSPCtXW5DlYyc3TJt69biOIxMNYcFnGcO2Ma5Q7F3PbVVYJXHhe', // Секретный ключ
-    redirect_uri: 'http://65.20.75.136.vultrusercontent.com/.netlify/functions/app/home', // Ссылка для перенаправления,
-    server : {
-      port: "3001"
-    }
-  },
-});
 
-
-
-router.get("/login", (req, res) => {
-  res.send(crm.connection.getAuthUrl())
-});
 
 router.get("/home", (req, res) => {
   res.redirect('https://myturkey.kz')
@@ -55,7 +34,27 @@ router.post('/request', async (req, res) => {
     const name = req.body.name;
     const phone = req.body.phone;
     
-    
+    const crm = new AmoCRM({
+      // логин пользователя в портале, где адрес портала domain.amocrm.ru
+      domain: 'aurorainc', // может быть указан полный домен вида domain.amocrm.ru, domain.amocrm.com
+      /* 
+        Информация об интеграции (подробности подключения 
+        описаны на https://www.amocrm.ru/developers/content/oauth/step-by-step)
+      */
+      auth: {
+        client_id: 'f8cef8e7-570a-458d-bd63-17d78b560f2e', // ID интеграции
+        client_secret: '4gWhvCDtNEkycxSPCtXW5DlYyc3TJt69biOIxMNYcFnGcO2Ma5Q7F3PbVVYJXHhe', // Секретный ключ
+        redirect_uri: 'https://6f93-2401-c080-2400-1e26-5400-3ff-fef6-ee7.jp.ngrok.io', // Ссылка для перенаправления,
+        server : {
+          port: "3001"
+        }
+      },
+    });
+
+    res.send(crm.connection.getAuthUrl())
+
+    console.log("here2")
+
     try {
         const contact = await crm.request.post( '/api/v4/contacts', [{
           "first_name": name,
